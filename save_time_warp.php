d3afc45d75f1914da764a4a010c3088f54c9e641
Chris Bartley <bartley@cmu.edu>
2011-04-06 13:00:57 -0400

    whoops, removed debug statements

--- save_time_warp.php
+++ save_time_warp.php
@@ -3,7 +3,5 @@ header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Sat, 1 Jan 2011 00:00:01 GMT");
 header('Content-type: application/x-warp');
 header('Content-Disposition: attachment; filename="'.$_POST["filename"].'"');
-error_log("filename = [".$_POST["filename"]."]");
-error_log("json = [".$_POST["json"]."]");
 ?>
 <?php echo $_POST["json"]; ?>
