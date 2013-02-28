5bea534ef52a47c23e11acae0e2c08e083bf07da
Paul Dille <pdille@cmucreatelab.org>
2013-02-28 13:51:39 -0500

    - CSS is now loaded before javascript files on included example embed pages, since Chrome profiling says this is preferred for performance. - Improved stalling workaround when manually time scrubbing. - Removed timeout on ajax calls, since we are still using only json files and the timeout would sometimes occur before everything loaded. - Increased controls/viewport default size.

--- js/org/gigapan/timelapse/videoset.js
+++ js/org/gigapan/timelapse/videoset.js
@@ -16,17 +16,17 @@
 // * jQuery (http://jquery.com/)
 //
 // Copyright 2011 Carnegie Mellon University. All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice, this list of
 //    conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice, this list
 //    of conditions and the following disclaimer in the documentation and/or other materials
 //    provided with the distribution.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ''AS IS'' AND ANY EXPRESS OR IMPLIED
 // WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 // FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY OR
@@ -36,7 +36,7 @@
 // ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 // The views and conclusions contained in the software and documentation are those of the
 // authors and should not be interpreted as representing official policies, either expressed
 // or implied, of Carnegie Mellon University.
@@ -408,7 +408,7 @@ if (!window['$']) {
 						// TODO: Should we check that the video actually stopped streaming?
             videoDiv.removeChild(inactiveVideos[id]);
           }
-          
+
           delete inactiveVideos[id];
           UTIL.log("video(" + id + ") garbage collected");
 
@@ -1052,8 +1052,9 @@ if (!window['$']) {
             //UTIL.log("current time " + video.getCurrentTime());
             //UTIL.log("leader " + leader);
             UTIL.log("video(" + videoId + ") time correction: seeking from " + (video.getCurrentTime() - leader) + " to " + t + " (error=" + error + ", state=" + video.readyState + ")");
-            // Since new stalling code seems to break timewarp playback where we emulate playback to handle going backwards or do lots of tweaking to handle different play rates, enter Mr. Hack-a-rooney.
-            if (!advancing)
+            // Allow manual frame stepping without stalling. If we are paused and not seeking, then there really is no reason to stall.
+            // Also fixes issues of stalling uncontrollably when playing in reverse.
+            if ((!advancing && video.seeking) || (advancing && !emulatingPlaybackRate))
               stall(true);
             var desiredTime = leader + t + (advancing ? playbackRate * errorThreshold * .5 : 0);  // seek ahead slightly if advancing
             try {
