CMU-CREATE-Lab/timemachine-viewer

commit 07328d279eed8c56b48f75cfe8567de4996dcc56

- Fixed edge case with the thumbnail timeline slider when it is at the end of video when first loaded.

js/org/gigapan/timelapse/defaultUI.js
@@ -381,10 +381,10 @@ if (!org.gigapan.timelapse.Timelapse) {
       $timelineSliderFiller.hide();
       var currentPos = timeToSliderValue(timelapse.getTimelapseCurrentTimeInSeconds());
       var rangePos = currentPos + timelineSelectorDefaultRangeOffset;
-      $timelineSelector.slider("values", 0, currentPos);
       $timelineSelector.slider("values", 1, rangePos);
-      $startingTimeSpinner.captureTimeSpinner("value", currentPos);
+      $timelineSelector.slider("values", 0, currentPos);
       $endingTimeSpinner.captureTimeSpinner("value", rangePos);
+      $startingTimeSpinner.captureTimeSpinner("value", currentPos);
       $startingTimeSpinner.captureTimeSpinner("option", "max", rangePos);
       $endingTimeSpinner.captureTimeSpinner("option", "min", currentPos);
       updateCaptureTimeRange($timelineSelector.slider("values", 0), $timelineSelector.slider("values", 1));

commit 88e3fdfe08555f1937d65e769c2e18f6e836d0c8

- Fixed regression of commit made in 07328d27

js/org/gigapan/timelapse/defaultUI.js
@@ -394,14 +394,14 @@ if (!org.gigapan.timelapse.Timelapse) {
       if (currentEndingIdx > timelapse.getNumFrames() - 1) {
         currentEndingIdx = timelapse.getNumFrames() - 1;
       }
-      $timelineSelector.slider("values", 0, currentStartingIdx);
       $timelineSelector.slider("values", 1, currentEndingIdx);
-      $startingTimeSpinner.captureTimeSpinner("option", "min", 0);
+      $timelineSelector.slider("values", 0, currentStartingIdx);
       $startingTimeSpinner.captureTimeSpinner("option", "max", currentEndingIdx);
-      $endingTimeSpinner.captureTimeSpinner("option", "min", currentStartingIdx);
+      $startingTimeSpinner.captureTimeSpinner("option", "min", 0);
       $endingTimeSpinner.captureTimeSpinner("option", "max", timelapse.getCaptureTimes().length - 1);
-      $startingTimeSpinner.captureTimeSpinner("value", currentStartingIdx);
+      $endingTimeSpinner.captureTimeSpinner("option", "min", currentStartingIdx);
       $endingTimeSpinner.captureTimeSpinner("value", currentEndingIdx);
+      $startingTimeSpinner.captureTimeSpinner("value", currentStartingIdx);
       updateCaptureTimeRange(currentStartingIdx, currentEndingIdx);
     };
     this.resetShareThumbnailUI = resetShareThumbnailUI;